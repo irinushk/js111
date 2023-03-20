22d2967e4b2fa06ce60736b017907145cb9e8540
irinushk <118196750+irinushk@users.noreply.github.com>
2023-03-20 14:20:08 +0400

    mzadaa

---
+++ script.js
@@ -0,0 +1,29 @@
+//problem 1
+
+let userName; 
+userName = "irina";
+//console.log (userName);
+
+let surName;
+surName = "chonkadze";
+//console.log (surName)
+
+let city;
+city = "Tbilisi"
+//console.log (city)
+
+const personalNumber = 123456;
+//console.log (personalNumber);
+
+//problem 2 
+// 9 Expressions
+
+//problem 3
+let number = 101%2;
+console.log (number);
+
+//ასეც შემეძლო ხო?
+//let x = 101;
+//let y = 2;
+//let z = x%y
+//console.log (z);
